f1bbc2fb3a410b3f6172e665f9d525c2e0daecf4
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-02-06 21:08:31 +0100

    Dodaj pliki projektów.

---
+++ BackgroundWorker/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BackgroundWorkerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.T2 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(99, 46);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(21, 13);
+            this.ResultLabel.TabIndex = 0;
+            this.ResultLabel.Text = "0%";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(142, 259);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(370, 259);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // T2
+            // 
+            this.T2.WorkerReportsProgress = true;
+            this.T2.WorkerSupportsCancellation = true;
+            this.T2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.T2_DoWork);
+            this.T2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.T2_ProgressChanged);
+            this.T2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.T2_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(329, 142);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.ComponentModel.BackgroundWorker T2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ BackgroundWorker/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if(!T2.IsBusy)
+            {
+                progressBar1.Visible = true;
+                T2.RunWorkerAsync();
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if(T2.WorkerSupportsCancellation)
+            {
+                T2.CancelAsync();
+            }
+        }
+
+        private void T2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+
+            for (int i  =  0; i <= 10; i++)
+            {
+                if(bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    
+                    System.Threading.Thread.Sleep(500);
+                    bw.ReportProgress(i * 10);
+                }
+            }
+        }
+
+        private void T2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ResultLabel.Text = (e.ProgressPercentage.ToString() + "%");
+            progressBar1.Increment(10);
+            progressBar1.Update();
+        }
+
+        private void T2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                ResultLabel.Text = "Cancelled!";
+                ResetProgressBar();
+            }
+            else if(e.Error != null)
+            {
+                ResultLabel.Text = "Error has occured";
+                ResetProgressBar();
+            }
+            else
+            {
+                ResultLabel.Text = "Done";
+                ResetProgressBar();
+            }
+        }
+
+        private void ResetProgressBar()
+        {
+            progressBar1.Visible = false;
+            progressBar1.Value = 0;
+            progressBar1.Update();
+        }
+    }
+}
